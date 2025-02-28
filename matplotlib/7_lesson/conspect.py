c69c94bb6ad979d5e3b4c58da10f2388729776a9
DobryySoul <neznaika1337@list.ru>
2025-02-28 12:16:51 +0300

    add 7 conspect

--- matplotlib/7_lesson/conspect.py
+++ matplotlib/7_lesson/conspect.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+from datetime import datetime
+
 # fig, ax = plt.subplots(2, 3)
 # fig, ax = plt.subplots(2, 3, sharex='col', sharey='row')
 
@@ -44,7 +46,82 @@ import matplotlib.pyplot as plt
 
 ## Поясняющие надписи
 
-births = pd.read_csv('births-1969.csv')
+# births = pd.read_csv('births-1969.csv')
+
+# births['day'] = births['day'].astype(int)
+
+# births.index = pd.to_datetime(10000*births.year +100*births.month + births.day, format='%Y%m%d')
+
+# # print(births.head())
+
+# births_by_date = births.pivot_table('births', [births.index.month, births.index.day])
+
+# print(births_by_date.head())
+
+# births_by_date.index = [
+#     datetime(1969, month, day) for month, day in births_by_date.index
+# ]
+
+# # print(births_by_date.head())
+
+# fig, ax = plt.subplots()
+# births_by_date.plot(ax=ax)
+
+# style = dict(size=10, color='gray')
+# ax.text('1969-01-01', 5500, 'Новый год', **style)
+# ax.text('1969-09-01', 4500, 'День знаний', ha='right')
+
+# ax.set(title='Рождаемость в 1969', ylabel="Число рождений")
+
+# ax.xaxis.set_major_formatter(plt.NullFormatter())
+# ax.xaxis.set_minor_formatter(mpl.date.DateFormatter('%h'))
+# ax.xaxis.set_minor_locator(mpl.dates.MonthLocator(bymonthday=5))
+
+# fig = plt.figure()
+# ax1 = plt.axes()
+
+# ax2 = plt.axes([0.4, 0.3, 0.1, 0.2])
+
+# ax1.set_xlim(0, 2)
+# ax1.text(0.6, 0.8, 'Data1 (0.6, 0.8)', transform=ax1.transData)
+# ax1.text(0.6, 0.8, 'Data2 (0.6, 0.8)', transform=ax2.transData)
+
+# ax1.text(0.5, 0.1, 'Data3 (0.5, 0.1)', transform=ax1.transAxes)
+# ax1.text(0.5, 0.1, 'Data4 (0.5, 0.1)', transform=ax2.transAxes)
+
+# ax1.text(0.2, 0.2, 'Data5 (0.2, 0.2)', transform=fig.transFigure)
+# ax2.text(0.2, 0.2, 'Data6 (0.2, 0.2)', transform=fig.transFigure)
+
+# fig, ax = plt.subplots()
+
+# x = np.linspace(0, 20, 1000)
+# ax.plot(x, np.cos(x))
+# ax.axis('equal')
+
+# ax.annotate('Локальный максимум', xy=(6.28, 1), xytext=(10,4), arrowprops=dict(facecolor='red'))
+
+# ax.annotate('Локальный минимум', xy=(3.14, -1), xytext=(2,-6), arrowprops=dict(facecolor='blue', arrowstyle='->'))
+
+# fig, ax = plt.subplots(4, 4, sharex=True, sharey=True)
+
+# for axi in ax.flat:
+#     axi.xaxis.set_major_locator(plt.MaxNLocator(5))
+#     axi.yaxis.set_major_locator(plt.MaxNLocator(3))
+
+# x = np.random.randn(1000)
 
 
+# fig = plt.figure(facecolor='gray')
+# ax = plt.axes(facecolor='green')
+
+# plt.grid(color='w', linestyle='solid')
+
+# ax.xaxis.tick_bottom()
+# ax.yaxis.tick_left()
+
+# with plt.style.context('bnh'):
+#     plt.hist(x)
+
+
+# .matplotlibrc
 plt.show()
